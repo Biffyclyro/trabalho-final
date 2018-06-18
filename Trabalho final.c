f06360ef997a8966ab5bcecbaef2c3cf2f7a7d6a
Biffyclyro <joao-senna1@hotmail.com>
2018-06-18 11:48:43 -0300

    Update Trabalho final.c

--- Trabalho final.c
+++ Trabalho final.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 #include<string.h>
+#define TAM 10
 
 
 struct alunos{
 	char nome[20], sobre[50], email[30];
-	int matricula, materias[6];
+	int matricula, materias[5]={};
 	
 };
 
 struct materias{
-	char nome[20], prof[30];
-	int carga, hora, cod;
+	char nome[20], dia[10];
+	int carga, hora[2], cod;
 	
 };
 
@@ -20,21 +21,28 @@ struct dia{
 };
 
 
-typedef struct dia d;
-typedef struct alunos al;
-typedef struct materias mat;
+typedef struct dia Dia;
+typedef struct alunos Alunos;
+typedef struct materias Materias;
 
-void cadastroAluno();
-void cadastroMateria(al aluno[10]);
+void cadastroAluno(Alunos aluno[TAM]);
+void cadastroMateria(Alunos aluno[TAM]);
+void materiaSemestre(Materias materia[5]);
 
 main(){
 	int i, j,x;
-	al aluno[10];
-	mat materia[10];
-	d dias[10];
-	cadastroAluno();
+	Alunos aluno[TAM];
+	Materias materia[5];
+	Dia dias[10];
+	cadastroAluno(aluno);
 	cadastroMateria(aluno);
-	
+	materiaSemestre(ateria);
+	printf("Entre com a opcao:\n")
+	printf("1. Consulta de aluno.\n");
+	printf("2. Cadastro de materia.\n");
+	printf("3. Impressão da grade.\n");
+	printf("4. Cadastro de aluno.\n");
+	scanf("%d",&x);
 	switch(x){
 		case 1:{
 			break;
@@ -48,20 +56,44 @@ main(){
 		case 3:{
 			break;
 		}
+		
+		case 4:{
+			break;
+		}
+		
 	}
 
 }
 
-void cadastroMateria(al aluno[10]){
-	int y=0;
-	printf("Entre com o c�digo da mat�ria: \n");
-	scanf("%d",al aluno[0].materias[0]);
-	printf("%d",al aluno[0].materias[0);
+void cadastroMateria(Alunos aluno[TAM]){
+	int y=0, x=0, i, j, index, matri;
+	
+	while(x==0)
+		printf("Entre com a matricola do aluno: \n");
+		scanf("%d",&matri);
+		printf("Entre com o código da matéria: \n");
+		scanf("%d",&y);
+		//scanf("%d",&aluno[0].materias[0]);
+		printf("%d",aluno[0].materias[0]);
+		for(j=0; j<TAM; j++){
+			if(aluno[j].matricula==matri)
+				index=j;
+		}
+		for(i=0, i<5, i++){			
+			if(aluno[index].materias[i]==y) printf("Materia ja cadastrada!\n");
+			if(aluno[index].materias[i]==127 && y == 132) printf("Horario de terca 13:00 ja ocupado!!\n");
+			if(aluno[index].materias[i]==132 && y == 127) printf("Horario de terca 13:00 ja ocupado!!\n");
+			if(aluno[index].materias[i]==143 && y == 135) printf("Horario de sexta 10:00 ja ocupado!!\n");
+			if(aluno[index].materias[i]==135 && y == 143) printf("Horario de sexta 10:00 ja ocupado!!\n");
+			if(aluno[index].materias[i]==0)aluno[index].materias[i]=y;
+			
+		}
+		
 		
 }
 
 
-void cadastroAluno(){
+void cadastroAluno(Alunos aluno[TAM]){
 	
 		
 	
@@ -75,3 +107,37 @@ void cadastroAluno(){
 	
 	
 }
+
+void materiaSemestre( Materias materia[5]){
+	strcpy(materia[0].nome, "Algorítmos");
+	materia[0].carga=60;
+	materia[0].hora[0]=8;
+	materia[0].hora[1]=10;
+	materia[0].cod=0122;
+	
+	strcpy(materia[1].nome, "Estrutura de dados");
+	materia[1].carga=60;
+	materia[1].hora[0]=13;
+	materia[1].hora[1]=15;
+	materia[1].cod=0127;
+	
+	strcpy(materia[2].nome, "Sistemas Operacionais A");
+	materia[2].carga=60;
+	materia[2].hora[0]=13;
+	materia[2].hora[1]=10;	
+	materia[2].cod=0132;
+	
+	strcpy(materia[3].nome, "Padroes de Projeto");
+	materia[3].carga=30;
+	materia[3].hora[0]=10;	
+	materia[3].cod=0143;
+	
+	strcpy(materia[4].nome, "Banco de dados II");
+	materia[4].carga=60;
+	materia[4].hora[0]=10;
+	materia[4].hora[1]=10;
+	materia[4].cod=0135;	
+	
+	
+}	
+	
